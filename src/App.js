97ccea54ae459342b7132bd5c1b6b34d921cdbee
Axedassis <lssvgabriel@gmail.com>
2022-03-12 11:27:04 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
-function App() {
+function App(){
+  const [result, setResult] = useState("")
+
+  const handleClick = (e) => {
+    if(e.target.id === 'c')
+      return setResult('')
+    if(e.target.id === 'clear')
+      return setResult(result.substring(0, result.length - 1)) 
+    if(e.target.id === 'result')
+     return setResult(eval(result.replace('x', '*').replace('÷', '/')).toString())
+    
+  
+    setResult(result.concat(e.target.name))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className='container'>
+      
+      <form>
+        <input type="text" value={result} onChange={e => e}  />
+      </form>
+        <div className='keypad'>
+        <button  id='clear'onClick={handleClick} className='color-button'>Clear</button>
+        <button  id='c' onClick={handleClick} className='color-button'>C</button>
+        <button name ="÷" onClick={handleClick} className='color-button'>&divide;</button>
+        <button name ="7" onClick={handleClick}>7</button>
+        <button name ="8" onClick={handleClick}>8</button>
+        <button name ="9" onClick={handleClick}>9</button>
+        <button name ="x" onClick={handleClick} className='color-button'>&times;</button>
+        <button name ="4" onClick={handleClick}>4</button>
+        <button name ="5" onClick={handleClick}>5</button>
+        <button name ="6" onClick={handleClick}>6</button>
+        <button name ="-" onClick={handleClick} className='color-button'>&ndash;</button>
+        <button name ="1" onClick={handleClick}>1</button>
+        <button name ="2" onClick={handleClick}>2</button>
+        <button name ="3" onClick={handleClick}>3</button>
+        <button name ="+" onClick={handleClick} className='color-button'>+</button>
+        <button name ="0" onClick={handleClick}>0</button>
+        <button name ="." onClick={handleClick} className='color-button'>.</button>
+        <button  id='result'onClick={handleClick} className='color-button'>=</button>
+        </div>
+
+      </div>
+    </>
   );
 }
 
